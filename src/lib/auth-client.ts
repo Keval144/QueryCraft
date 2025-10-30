4dea21cae15d90194cc0d531f5b2c63d4c6556e2
keval kansagra <kevalm144@gmail.com>
2025-10-30 12:40:34 +0530

    auth plugin

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,7 @@
 import { createAuthClient } from "better-auth/client";
 import { adminClient, lastLoginMethodClient } from "better-auth/client/plugins";
+import { nextCookies } from "better-auth/next-js";
 
 export const authClient = createAuthClient({
-  plugins: [adminClient(), lastLoginMethodClient()],
+  plugins: [adminClient(), lastLoginMethodClient(),nextCookies()],
 });
