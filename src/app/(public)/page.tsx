1f0d2e81c29851e40449f1bf3c8076d0a04c59c2
keval kansagra <kevalm144@gmail.com>
2025-10-31 21:05:04 +0530

    Colour improvs

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,17 +1,17 @@
 import { AppNavbar } from "@/components/common/app-navbar";
 
-function page() {
+function Page() {
   return (
-    <>
-      <div className="relative min-h-dvh w-full bg-white">
-        <div className="absolute inset-0 z-0 bg-[radial-gradient(125%_125%_at_50%_90%,#ffffff_40%,#1d9bf0_100%)] bg-size-[100%_100%] transition-all duration-700 ease-in-out dark:bg-[radial-gradient(125%_125%_at_50%_90%,#0a0c10_40%,#2563eb_100%)]" />
-
-        <div className="relative z-10">
-          <AppNavbar />
-        </div>
+    <div className="relative w-full">
+      {/* only one screen height worth of gradient */}
+      <div className="relative h-screen bg-[radial-gradient(125%_125%_at_50%_90%,#ffffff_40%,#1d9bf0_100%)] transition-all duration-700 ease-in-out dark:bg-[radial-gradient(125%_125%_at_50%_90%,#15181c_40%,#2563eb_100%)]">
+        <AppNavbar />
       </div>
-    </>
+
+      {/* normal content below */}
+      <div className="bg-background h-[400px]"></div>
+    </div>
   );
 }
 
-export default page;
+export default Page;
